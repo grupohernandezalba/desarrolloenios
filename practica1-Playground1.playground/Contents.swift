142a94069fc9b70f8f85c565ac8e1ae502c2af2c
grupohernandezalba <marco@grupohernandezalba>
2020-12-12 11:25:53 -0600

    practica1 de Playground

---
+++ practica1-Playground1.playground/Contents.swift
@@ -0,0 +1,55 @@
+import UIKit
+
+
+// Strings, asignación implícita
+var cadena = "Hola mundo"
+    cadena = "Marco Hernández"
+
+//Asignación explícita
+var apellido:String = "Hola mundo"
+
+// Constant
+let correo = "marco@grupohernandezalba.com"
+    //correo = "info@grupohernandezalba.com"
+
+// Integer : Int
+var entero = 1
+
+var a:Int = 10
+var b:Int = 20
+
+//Operadores aritmeticos básicos
+
+var suma:Int = a + b
+var resta:Int = a - b
+var multiplicacion:Int = a * b
+
+var pi:Double = 3.1416
+var x:Double = 10
+var y:Double = 10
+var division:Double = x / y
+var radio:Double = 1.5
+var area: Double = pi * (radio * radio)
+
+let π = 3.14159
+let 你好 = "你好世界"
+let 🐶🐮🐮 = "dogcowcow"
+
+
+// Double Float
+var flotante = 1.5
+
+//Imprimiendo variables y constantes
+
+print(area)
+
+// Imprimiendo variables
+
+print("El valor del área es: \(area)")
+
+// Punto y coma
+
+var i:Int = 0; var j:Int = 0
+
+// Limites 
+
