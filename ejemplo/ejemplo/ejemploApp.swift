748fed748514a0c6393c2e46223b387707bd7db6
Marco Antonio Hernández Alba <marco@grupohernandezalba.com>
2021-02-27 09:12:30 -0600

    se agrego ejemplo

---
+++ ejemplo/ejemplo/ejemploApp.swift
@@ -0,0 +1,17 @@
+//
+//  ejemploApp.swift
+//  ejemplo
+//
+//  Created by Marco Antonio Hernández Alba on 27/02/21.
+//
+
+import SwiftUI
+
+@main
+struct ejemploApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
