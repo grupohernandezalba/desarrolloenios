81969525ca508415f2109dd861b93b04f8f46235
Marco Antonio Hernández Alba <marco@grupohernandezalba.com>
2021-03-04 18:04:16 -0600

    practica10

--- Practicas/practica10-genericos.playground/Contents.swift
+++ Practicas/practica10-genericos.playground/Contents.swift
@@ -1,58 +1,51 @@
 import UIKit
 
-var texto:String = "Hello world!"
+var texto:String = "Hola Mundo"
+
 //texto = 5
 
 
-func suma(a: Int, b: Int) -> Int
-{
+func suma(a: Int, b: Int) -> Int {
     return a + b
 }
 
 let resultado = suma(a: 42, b: 99)
+
 print(resultado)
 
 
-func suma2(a: Double, b: Double) -> Double
-{
+func suma2(a: Double, b: Double) -> Double {
     return a + b
 }
 
 let resultado2 = suma2(a: 42.5, b: 99.3)
+
 print(resultado2)
 
-//Principio DRY
-
-func sumaGenerica<T: Numeric>(a: T, b: T) -> T
-{
+func sumaGenerica<T: Numeric> (a: T, b: T) -> T {
     return a + b
 }
 
-print(sumaGenerica(a:42,b:99))
-print(sumaGenerica(a:42.5,b:99.3))
+print(sumaGenerica(a: 42, b: 99))
+print(sumaGenerica(a: 42.5, b: 99.3))
 print(sumaGenerica(a: Double.pi, b: Double.pi))
 
-var dato = sumaGenerica(a:42,b:99)
-print("Dato \(dato)")
 
-//Generic Type Constraints
 //Equatable
-let arregloNombres = ["Hugo", "Paco", "Luis"]
+let arregloNombres = ["Hugo","Paco","Luis"]
 
-for (index, nombre) in arregloNombres.enumerated() {
-    print("\(index): '\(nombre)'")
+for(index, nombre) in arregloNombres.enumerated() {
+    
+    print("\(index): \(nombre)")
+    
 }
 
-
-func obtienePosicion<T:Equatable>(abuscar: T, nombres: [T]) -> Int?
-{
-    for (index, nombre) in nombres.enumerated()
-    {
+func obtienePosicion <T:Equatable>(abuscar: T, nombres: [T] ) -> Int? {
+    for(index, nombre) in nombres.enumerated() {
         if nombre == abuscar {
             return index
         }
     }
-
     return nil
 }
 
@@ -60,21 +53,24 @@ func obtienePosicion<T:Equatable>(abuscar: T, nombres: [T]) -> Int?
 let encontrado = obtienePosicion(abuscar: "Luis", nombres: arregloNombres)
 
 if encontrado != nil {
-    print("Se encontró en la posición: ")
+    print("Se encontró en la posición:")
     print(encontrado!)
 }
-else
-{
-    print("No se encontró.")
+else{
+    print("No se encontró")
 }
 
-//Extensión de estructruras
+
+//Extensión de estructuras
+
+
 struct Point {
-    let x: Int, y: Int
+    let x:Int, y:Int
 }
 
-let p = Point(x: 21, y: 30)
+let p = Point(x:21, y:30)
 print(p)
+
 
 extension Point {
     var descripcion: String {
@@ -82,24 +78,22 @@ extension Point {
     }
 }
 
-var x = p.descripcion
-print(x)
+var nueva = p.descripcion
+print(nueva)
 
- 
-//Overriding methods
 
 class Perro {
-    var raza:String = ""
-    
-    func haceRuido() {
+    var raza: String = ""
+    func haceRuido(){
         print("Woof!")
     }
+    
 }
 
 class Cachorro: Perro {
     
     override func haceRuido() {
-        print("Yip!")
+        print("Yiip!")
     }
     
 }
@@ -109,48 +103,68 @@ mascota.raza = "Pastor Alemán"
 
 mascota.haceRuido()
 
-
+//final
 
 //final class Paciente {
 class Paciente {
-    var nombre : String
-    var padecimiento : String
+    var nombre: String
+    var padecimiento: String
     
-    init(nombre: String, padecimiento: String) {
+    init(nombre:String,padecimiento:String) {
         self.nombre = nombre
         self.padecimiento = padecimiento
     }
     
 }
 
-class EnfermoGrave: Paciente {
-    var nivelDolor: Int
+let objetoPaciente = Paciente(nombre: "Carlos Pérez", padecimiento: "Hipertensión")
+print(objetoPaciente.nombre)
+
+class EnfermoGrave: Paciente  {
+    var nivelDolor : Int
     
-    init(nombre: String, padecimiento: String , nivelDolor: Int) {
+    init(nombre:String, padecimiento:String, nivelDolor: Int){
         self.nivelDolor = nivelDolor
         super.init(nombre: nombre, padecimiento: padecimiento)
-        //self.nivelDolor = nivelDolor
+        
     }
 }
 
-let objetoEnfermo:EnfermoGrave = EnfermoGrave(nombre:"Juan", padecimiento: "Diabetes", nivelDolor: 3)
+let objetoEnfermo:EnfermoGrave = EnfermoGrave(nombre: "Juan Lopez", padecimiento: "Diabetes", nivelDolor: 3)
 
 print("\(objetoEnfermo.nombre) \(objetoEnfermo.nivelDolor)")
 
 
-//Copia de objetos
-//struct Cantante {
-class Cantante {
+struct Cantante {
     var nombre = "Carlos Rivera"
 }
 
 var objetoCantante:Cantante = Cantante()
+
 print(objetoCantante.nombre)
 
 var objetoCantante2 = objetoCantante
+
 print(objetoCantante2.nombre)
 
 objetoCantante2.nombre = "Christian Nodal"
 
 print(objetoCantante.nombre)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
