f3a433f4589a395ded330430a997ef8e7de206b9
Marco Antonio Hernández Alba <marco@grupohernandezalba.com>
2021-03-05 22:54:53 -0600

    Proyecto Restaurant

---
+++ Proyectos/Restaurant/Restaurant/RestaurantApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantApp.swift
+//  Restaurant
+//
+//  Created by Marco Antonio Hernández Alba on 05/03/21.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
