3c4e2e3f055f409d10991de7a87143d907b57fae
Marco Antonio Hernández Alba <marco@grupohernandezalba.com>
2021-03-11 13:16:29 -0600

    ciclodevida

---
+++ Proyectos/SplashIcono/SplashIcono/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  SplashIcono
+//
+//  Created by Marco Antonio Hernández Alba on 08/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .gray
+        
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Proyectos/cicloApp/cicloApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cicloApp
+//
+//  Created by Marco Antonio Hernández Alba on 10/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
