0485229cc5112bb7f5132a58e484038f9f35e0fc
Marco Antonio Hernández Alba <marco@grupohernandezalba.com>
2021-03-19 12:42:46 -0600

    autolayout

---
+++ Proyectos/Stacks-2/Stacks-2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Stacks-2
+//
+//  Created by Marco Antonio Hernández Alba on 18/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
